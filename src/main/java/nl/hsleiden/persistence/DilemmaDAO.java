b4c73c902116d7fdce157cea5e5b0273a606d394
DennisvanBeelen <39301585+DennisvanBeelen@users.noreply.github.com>
2019-01-16 14:52:19 +0100

    back-end voor dilemma bijna af

--- src/main/java/nl/hsleiden/persistence/DilemmaDAO.java
+++ src/main/java/nl/hsleiden/persistence/DilemmaDAO.java
@@ -5,6 +5,7 @@ import nl.hsleiden.model.Dilemma;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,17 +13,75 @@ public class DilemmaDAO {
 
     private Database database;
 
-    public DilemmaDAO(){
+    public DilemmaDAO() {
 
     }
 
-    public List<Dilemma> getAll(){
+    public List<Dilemma> getAll() {
         List<Dilemma> dilemmas = new ArrayList<>();
         String SQL = "SELECT*FROM dilemma";
         PreparedStatement statement = null;
         ResultSet rs = null;
+
+        try {
+            statement = this.database.connect().prepareStatement(SQL);
+            rs = statement.executeQuery();
+            while (rs.next()) {
+                dilemmas.add(
+                        new Dilemma(
+                                rs.getInt("id"),
+                                rs.getString("subject"),
+                                rs.getBoolean("pregnant"),
+                                rs.getInt("week_no")
+                        )
+                );
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                rs.close();
+                statement.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println(dilemmas);
+        return dilemmas;
     }
 
+    public void setDatabase(Database database) {
+        this.database = database;
+    }
 
+    public Dilemma get(int id) {
+        Dilemma dilemma = null;
+        String SQL = "SELECT * FROM dilemma WHERE id = ?";
+        PreparedStatement statement = null;
+        ResultSet rs = null;
+
+        try {
+            statement = this.database.connect().prepareStatement(SQL);
+            statement.setInt(1, id);
+            rs = statement.executeQuery();
+            while (rs.next()) {
+                dilemma = new Dilemma();
+                dilemma.setDilemmaId(rs.getInt("id"));
+                dilemma.setSubject(rs.getString("subject"));
+                dilemma.setPregnant(rs.getBoolean("pregnant"));
+                dilemma.setWeekNr(rs.getInt("week_no"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                rs.close();
+                statement.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return dilemma;
+    }
 
 }
--- src/main/java/nl/hsleiden/service/DilemmaService.java
+++ src/main/java/nl/hsleiden/service/DilemmaService.java
@@ -1,4 +1,29 @@
 package nl.hsleiden.service;
 
-public class DilemmaService {
+import nl.hsleiden.ApiApplication;
+import nl.hsleiden.model.Dilemma;
+import nl.hsleiden.persistence.DilemmaDAO;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+import java.util.Collection;
+
+@Singleton
+public class DilemmaService extends BaseService<Dilemma>{
+
+    private final DilemmaDAO dao;
+
+    @Inject
+    public DilemmaService(DilemmaDAO dao){
+        this.dao = dao;
+        this.dao.setDatabase(ApiApplication.getDatabase());
+    }
+
+    public Collection<Dilemma> getAll(){
+        return dao.getAll();
+    }
+
+    public Dilemma get(int id){
+        return requireResult(dao.get(id));
+    }
 }
