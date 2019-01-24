32825d1ce18885f2c6ee88098e1e1f3d4b1b0416
fleurvaneijk <s1102696@student.hsleiden.nl>
2019-01-24 12:54:45 +0100

    statistics dao

---
+++ src/main/java/nl/hsleiden/persistence/StatisticsDAO.java
@@ -0,0 +1,59 @@
+package nl.hsleiden.persistence;
+
+import nl.hsleiden.database.Database;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * DAO that gets statistics data from multiple tables.
+ * @author Fleur van Eijk
+ */
+public class StatisticsDAO {
+
+    private Database database;
+    private ResultSet resultSet = null;
+
+    public StatisticsDAO() {
+
+    }
+
+    public List getRatingPerSubject() {
+        ArrayList row = new ArrayList();
+        ArrayList ratingPerSubjectList = new ArrayList();
+
+        try {
+            String query =  "SELECT couple.pregnant, dilemma.subject, SUM(rating_dilemma) AS rating FROM rating JOIN dilemma ON(dilemma_id = id) JOIN couple ON(parent_email = parent_email_1 OR parent_email = parent_email_2) GROUP BY couple.pregnant, dilemma.subject";
+
+            PreparedStatement statement = database.getConnection().prepareStatement(query);
+            resultSet = statement.executeQuery();
+
+            while (resultSet.next())
+            {
+                row.add(resultSet.getBoolean("pregnant"));
+                row.add(resultSet.getString("subject"));
+                row.add(resultSet.getInt("rating"));
+
+                ratingPerSubjectList.add(row);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        finally {
+            try {
+                resultSet.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return ratingPerSubjectList;
+    }
+
+    public void setDatabase(Database database) {
+        this.database = database;
+    }
+
+}
---
+++ src/main/java/nl/hsleiden/resource/StatisticsResource.java
@@ -0,0 +1,42 @@
+package nl.hsleiden.resource;
+
+import com.fasterxml.jackson.annotation.JsonView;
+import com.google.inject.Singleton;
+import nl.hsleiden.View;
+import nl.hsleiden.service.StatisticsService;
+
+import javax.annotation.security.RolesAllowed;
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.util.Collection;
+import java.util.List;
+
+/**
+ * @author Fleur van Eijk
+ */
+@Singleton
+@Path("/statistics")
+@Produces(MediaType.APPLICATION_JSON)
+public class StatisticsResource {
+
+    private final StatisticsService service;
+
+    @Inject
+    public StatisticsResource(StatisticsService service)
+    {
+        this.service = service;
+    }
+
+    @GET
+    @Path("/ratingPerSubject")
+    @JsonView(View.Public.class)
+//    @RolesAllowed({"ADMIN", "MEDEWERKER"})
+    public List retrieveRatingPerSubject()
+    {
+        return service.getRatingPerSubject();
+    }
+
+}
